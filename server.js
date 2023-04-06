61d7ac1f9046ff53f9dd308fe76fd6226a4e37c9
Jason <jasondal91@gmail.com>
2023-04-06 17:47:59 +0900

    server 연결

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(
+  '/static',
+  express.static(path.resolve(__dirname, 'frontend', 'static'))
+);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+app.listen(5500, () => console.log('Server started on port 5500'));
